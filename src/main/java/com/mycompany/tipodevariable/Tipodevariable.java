120e6a6a5a79185399695ea9df5e93ce4f6887bd
maryuri-beatz <maryuribeatrizgarcia@gmail.com>
2022-08-29 14:59:56 -0600

    primer commit

---
+++ src/main/java/com/mycompany/tipodevariable/Tipodevariable.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.tipodevariable;
+
+/**
+ *
+ * @author maryuri
+ */
+// variables .java
+public class Tipodevariable {
+
+    public static void main(String[] args) {
+        //Declarar la variable speed de tipo int (numeros enteros);
+        int spped;
+        
+        // Actualizar el contenido de la variable spped:
+        spped = 10; // si ya habias declarado la variable 
+        
+        // Declarar una variable y asignarle un valor al mismo tiempo:
+        int salary = 1000;
+        
+        // crear una variable de tipo String:
+        String eployeeName = "Maryuri Beatriz";
+        
+    }
+}
+
+
+
